8a33b5c74903bdb343bba4500203a72feabeb5f2
ARedkozubova <57895723+ARedkozubova@users.noreply.github.com>
2019-12-01 17:34:08 +0300

    Update sl_list.c
    Added realisation of functions:
    slist_new
    slist_delete (//TODO)
    slist_insert
    slist_remove

--- sl_list/sl_list.c
+++ sl_list/sl_list.c
@@ -8,26 +8,80 @@
 #include <stdio.h>
 #include "sl_list.h"
 
+/* Create new empty list */
 pslist slist_new(void)
 {
-	return NULL;
+	pslist list = malloc(sizeof(struct slist));
+	if (NULL == list)
+	{
+		printf("ERROR: failed allocation of memory for new list!\n");
+		return NULL;
+	}
+	list->list_size = 0;
+	list->head = NULL;
+
+	return list;
 }
 
 /* For each element free memory */
-void slist_delete(pslist list)
-{ }
+/*void slist_delete(pslist list)
+{
+	pslist_entry head;
+	while (NULL != (head = slist_pop(list))) //TODO: slist_pop function should be done
+	{
+		free(head);
+	}
+
+	free(list);
+}*/
 
 /* Allocate the element
    Put at the end */
 int slist_insert(pslist list, int value)
 {
+	struct slist_entry *pnew = malloc(sizeof(struct slist_entry));
+	if (NULL == pnew)
+	{
+		printf("ERROR: failed allocation of memory for new element!\n");
+		return -1;
+	}
+	pnew->next = list->head;
+	pnew->value = value;
+	printf("New element: %d\n", pnew->value);
+	list->head = pnew;
+	list->list_size++;
 	return 0;
 }
 
 /* Go through the list, remove if value is equal */
 int slist_remove(pslist list, int value)
 {
-	return 0;
+	pslist_entry current = list->head;
+	pslist_entry prev = NULL;
+	if (NULL == current)
+	{
+		printf("ERROR: removing element from empty list!\n");
+		return -1;
+	}
+
+	while (current != NULL)
+	{
+		if (current->value == value)
+		{
+			while (prev->next != current)
+			{
+				prev = prev->next;
+			}
+
+			prev->next = current->next;
+			free(current);
+			if (NULL != current)
+			{
+				printf("ERROR: memory was not freed!\n");
+				return -2;
+			}
+		}
+	}
 }
 
 /* For each element print in value */
