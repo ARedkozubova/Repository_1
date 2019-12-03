541ad871a04756b537b45a181f3db52c6835eb61
kasapenkonata <58269792+kasapenkonata@users.noreply.github.com>
2019-12-03 18:25:58 +0300

    Update sl_tests.c
    Added test3() - checks function list_delete() OK
    Added test4() - checks function list_remove() -doesn't work

--- sl_list/sl_tests.c
+++ sl_list/sl_tests.c
@@ -8,35 +8,81 @@
 
 #include <stdio.h>
 #include "sl_list.h"
-#include < assert.h > 
 
-
-/*
-* test1
-* cheking work of function slist_new()
-*/
+ /*
+ * test1
+ * cheking work of function slist_new()
+ */
+//works OK
 int test1()
 {
 	pslist list;
 
 	list = slist_new();
 
-	if (NULL != list)
-	{
-		printf("List created! %p\n", list);
-	}
+	if (NULL != list->head)
+		goto errorpath;
+	if (NULL != list->list_size)
+		goto errorpath;
 
-	else
-	{
-		printf("ERROR: List was not created!");
-	}
+	printf("List Created!");
+
+	return 0;
+
+errorpath:
+	return 1;
+	printf("Error: mistakes in creatin the list");
 }
 
 /*
 * test2
 * checking work of function slist_insert() and slist_print()
 */
+//works OK
 int test2()
+{
+	pslist list;
+
+	int result = 0;
+
+	list = slist_new();
+
+	slist_print(list);  //prints an empty list - OK
+
+	result = result + slist_insert(list, 1);
+
+	if (1 != list->list_size)
+		goto errorpath;
+
+	slist_print(list);
+	printf("\n");
+
+	result = result + slist_insert(list, 2);
+	if (2 != list->list_size)
+		goto errorpath;
+
+	slist_print(list);
+	printf("\n");
+
+	result = result + slist_insert(list, 3);
+	if (3 != list->list_size)
+		goto errorpath;
+
+	slist_print(list);
+	printf("\n");
+
+	return result;
+
+errorpath:
+	result = 1;
+}
+
+/*
+* test3()
+* checks work of function list_delete();
+*/
+//works OK/ 
+int test3()
 {
 	pslist list;
 
@@ -44,17 +90,22 @@ int test2()
 
 	slist_insert(list, 1);
 	slist_insert(list, 2);
-	slist_insert(list, 3);
+
+	slist_print(list);
+	printf("\n");
+
+	slist_delete(list);
 
 	slist_print(list);
 
 	return 0;
-} 
+}
 
 /*
 * test4
 * cheking work of function slist_remove()
 */
+//got ann error/ programme crushes
 int test4()
 {
 	pslist list;
@@ -63,14 +114,17 @@ int test4()
 
 	slist_insert(list, 1);
 	slist_insert(list, 2);
-	slist_insert(list, 3);
+	slist_insert(list, 4);
+	slist_insert(list, 2);
 
-	printf("\ntest4:\n");
+	slist_print(list);
+	printf("\n");
 
-	slist_remove(list,2);
+	slist_remove(list, 2);
+
+	slist_print(list);
 
 	return 0;
 }
-
 
 
