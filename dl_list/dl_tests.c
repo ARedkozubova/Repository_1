2bceca8328073d83b7ddcfa78bafba7671dacd81
ARedkozubova <57895723+ARedkozubova@users.noreply.github.com>
2019-12-03 18:42:53 +0300

    Add tests
    Added realisation of tests for function:
    list_new()
    push()
    Added additional function find_element

--- dl_list/dl_tests.c
+++ dl_list/dl_tests.c
@@ -2,41 +2,84 @@
  * File: dl_list
  * Brief: Double linked list header
  * Author: Anastasia Redkozubova
- * Created on: 18.11.2019
+ * Created on: 03.12.2019
  * (c) MIPT
 */
 
-#include "pch.h"
 #include <stdio.h>
 #include "dl_list.h"
 
-int test1()
+
+int find_element(List *list, int value)
 {
-	LIST *list = list_new();
+	int flag = 0;
+	Node *current = list->head;
+	while (NULL != current)
+	{
+		if (current->value == value)
+			flag = 1;
+		current = current->next;
+	}
+	return flag;
+}
+
+
+int test_new()
+{
+	List *list;
+	list = list_new();
 	if (NULL == list)
 	{
-		printf("test1: list is not empty!\n");
+		printf("ERROR in test_new: creation of list is incorrect!\n");
 		return -1;
 	}
-	if (NULL == list->first)
+	if (NULL != list->head)
 	{
-		printf("test2: first element is not empry!\n");
+		printf("%ERROR in test_new: first element is not NULL!\n");
 		return -2;
 	}
-
-
+	if (NULL != list->tail)
+	{
+		printf("%ERROR in test_new: last element is not NUL!L\n");
+		return -3;
+	}
+	else
+	{
+		printf("PASS: test_new\n");
+		return 0;
+	}
 	list_delete(list);
-	return 0;
 }
 
-int test2()
+int test_push()
 {
-	return 0;
-}
-
-int run_all_tests()
-{
-	(test1 == 0) ? printf("PASS\n") : printf("FAULT\n");
-	(test2 == 0) ? printf("PASS\n") : printf("FAULT\n");
-	return 0;
+	List *list;
+	list = list_new();
+	int i = 0, count = 0, flag = 1;
+	for (i = 0; i < 20; i++)
+	{
+		push(list, i);
+	}
+	for (i = 0; i < 20; i++)
+	{
+		if (find_element(list, i))
+			count++;
+		else
+			flag = 0;
+	}
+	if(!flag)
+	{
+		printf("ERROR in test_push: element was not pushed!\n");
+		return -1;
+	}
+	if (count != list->size)
+	{
+		printf("ERROR in test_push: amount of element is not equal size of list!\n");
+		return -2;
+	}
+	else
+	{
+		printf("PASS: test_push\n");
+		return 0;
+	}
 }
--- dl_list/dl_tests.h
+++ dl_list/dl_tests.h
@@ -2,7 +2,7 @@
  * File: dl_list
  * Brief: Double linked list header
  * Author: Anastasia Redkozubova
- * Created on: 18.11.2019
+ * Created on: 03.12.2019
  * (c) MIPT
 */
 
@@ -10,8 +10,24 @@
 #include <stdio.h>
 #include "dl_list.h"
 
-int test1();
 
-int test2();
+/*
+* Supporting function for other tests
+* Check if element is in the list
+* Params: list - pointer to the list, element - int value
+* Return:
+*		0 if element is not in the list
+*		1 if element is in the list
+*/
+int find_element(List *list, int value);
 
-int run_all_tests();
+/*
+* Test for cheking function list_new
+*/
+int test_new();
+
+/*
+* Test for cheking function push
+*/
+int test_push();
+
