03ae4810c1dac6313ee9ffc0e685d027bae49ade
kasapenkonata <58269792+kasapenkonata@users.noreply.github.com>
2019-12-09 21:03:36 +0300

    Create sl_list.h

---
+++ sl_list/sl_task.h
@@ -0,0 +1,14 @@
+/*
+File: task.h
+Header to task
+Created on 09.12.19
+Author: Kasapenko Natalya
+*/
+
+/*
+function plist first_integers(int N) creates 
+single linked list of N elements that decrease from N to 1 (N is in head of the list)
+arguments: int N(amount of elements)
+returning value plist(Link to the list)
+*/
+pslist first_integers(int N);
