5396be5479d66c3adb00c57beba08bcb4e5ae0e3
kasapenkonata <58269792+kasapenkonata@users.noreply.github.com>
2019-12-15 02:06:33 +0300

    Added tests.h

---
+++ bst/tests.h
@@ -0,0 +1,44 @@
+/*
+File: tests.h
+description for tests for Binary Tree
+Created on: 15.12.19
+Author: Kasapenko Natalya
+*/
+
+/*
+* test1()
+* checks work of function new_tree()
+* returns 0 if everything is OK, returns 1 if error found
+*/
+test1();
+
+/*
+* test3(), test6(), test7()
+* checks work of function add_adge()
+* returns 0 if everything is OK, returns 1 if error found
+*/
+test3(); 
+test6();
+test7();
+
+/*
+* test4(), test5()
+* checks work of function delete_tree()
+* returns 0 if everything is OK, returns 1 if error found
+*/
+test4();
+test5();
+
+/*
+* test8()
+* checks work of function wfs()
+* returns 0 if everything is OK, returns 1 if error found
+*/
+test8();
+
+/*
+* test8()
+* checks work of function dfs()
+* returns 0 if everything is OK, returns 1 if error found
+*/
+test9();
